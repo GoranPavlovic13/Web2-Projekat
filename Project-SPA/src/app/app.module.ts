3dbe2d448e3dc265cd10ed398849d09a3adc90a6
Nikola Jakovljevic <nikola.jakovljevicr1@gmail.com>
2020-08-14 19:29:03 +0200

    Home page created and navbar routing implemented

--- Project-SPA/src/app/app.module.ts
+++ Project-SPA/src/app/app.module.ts
@@ -2,13 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './user/register/register/register.component';
+import { LoginComponent } from './user/login/login.component';
+import { RouterModule } from '@angular/router';
+import { appRoutes} from './routes';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [		
+    AppComponent,
+      HeaderComponent,
+      HomeComponent,
+      RegisterComponent,
+      LoginComponent
+   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
